3965d88f1a10a03a9cbf9cc35c94e70c3fc23e13
SolWeintraub95 <shutov9597@gmail.com>
2025-10-07 19:36:35 +0300

    task 414 not done

--- 414_third_max_number.py
+++ 414_third_max_number.py
@@ -1,15 +1,23 @@
 class Solution:
     def max(self, nums: list[int]) -> int:
-        pass
+        max = -2**31-1
+        for i in range(len(nums)):
+            if nums[i] > max:
+                max = nums[i]
+        return max
 
     def thirdMax(self, nums: list[int]) -> int:
-        pass
+        max=max2=max3 = -2**31-1
+        min = 2**31-1
 
+        for i in range(len(nums)):
+            if nums[i] > max:
+                max = max2 = nums[i]
+            if nums[i] < max:
+                max2 = nums[i]
+        print(max,max2)
+        #print(max,max2,max3)
 
-
-
-
-
-
-result = Solution().thirdMax(3,1,2)
-print(result)
+#result = Solution().thirdMax(3,1,2)
+result = Solution().thirdMax([1,2,3,4])
+#print(result)
