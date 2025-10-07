4be8f3321a16c8f46147d5194fa53fcce7d9d1b6
SolWeintraub95 <shutov9597@gmail.com>
2025-10-07 04:58:12 +0300

    task 242 v2 done

--- 242_valid_anagramm_v2.py
+++ 242_valid_anagramm_v2.py
@@ -3,12 +3,33 @@ class Solution:
         if len(s) != len(t):
             return False
         d = {}
-        for i in range(len(s)):
-            if s[i] in d:
+
+        for ch in s:
+            if ch not in d:
+                d[ch] = 0
+
+        for ch in s:
+            d[ch]+= 1
+
+        for ch in t:
+            if ch in d:
+                d[ch]-= 1
+
+        if all(value == 0 for value in d.values()):
+            return True
+        else:
+            return False
+
+        #print(d)
+
+
+
+        '''for i in range(len(s)):
+            if s[i] not in d:
                 d[s[i]] = 1
             else:
                 d[s[i]]+= 1
-            print(d)1
+            print(d)1'''
 
 
 
