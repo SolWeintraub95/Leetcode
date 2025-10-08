9bec0b4a3274dbdd911b64bcd942678d785391fc
SolWeintraub95 <shutov9597@gmail.com>
2025-10-08 21:43:27 +0300

    414 v1 my solution with costily delete some garbage and add some explanation commentary

--- 414_third_max_number.py
+++ 414_third_max_number.py
@@ -1,5 +1,6 @@
 class Solution:
     def thirdMax(self, nums: list[int]) -> int:
+        #Костыль
         max=max2=max3 = -2**31-1
         min = nums[0]
         #Запускаем счётчик по количеству элементов в листе и стандартное сравнение что каждый элемент массива меньше max
@@ -10,21 +11,19 @@ class Solution:
         for i in range(len(nums)):
             if nums[i] < min:
                 min = nums[i]
-        #print(max,min)
-        #Запускаем тот же счётчик. max2 должен быть (строго?) меньше max, больше минимального числа int
+
+        #Запускаем тот же счётчик. max2 должен быть строго меньше max, больше минимального числа int
         for i in range(len(nums)):
             if nums[i] > max2 and nums[i] < max:
                 max2=nums[i]
-        #print(max,max2)
+
 
         # Запускаем тот же счётчик. max3 должен быть (строго?) меньше max2, больше минимального числа int
         for i in range(len(nums)):
             if nums[i] > max3 and nums[i] < max2:
                 max3=nums[i]
-        #print(max,max2,max3)
-        #print(max,max2,max3)
+
         #Костыли для решения [1,2], [1,1,2] и [1,1,1] соответственно
-        #Переделать в паре мест ХУЙ ХУЙ ХУЙ
         if len(nums)<=2 or min == max2 or min==max:
             return max
         return max3
