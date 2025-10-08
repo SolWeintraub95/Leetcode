fcb3f3f45c2fac737b687a9fcdfd938a6b3816b6
SolWeintraub95 <shutov9597@gmail.com>
2025-10-08 23:48:11 +0300

    done 392 with Pete and Dmytro

---
+++ 392_is_subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        j = i = 0
+        #Если мы в итоговой строке встречаем символ из первой - удаляем его из листа и продолжаем идти по массиву t
+        if not s:
+            return True
+        while j < len(t):
+            if s[i] == t[j]:
+                j+=1
+                i+=1
+                if i == len(s):
+                    return True
+            else:
+                j+=1
+        return False
+
+result = Solution().isSubsequence('b', 'c')
+print(result)
