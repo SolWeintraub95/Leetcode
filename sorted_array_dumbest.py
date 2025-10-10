ea243eeb6f54a2a2b973dcf9bbcba22648d22c1d
SolWeintraub95 <shutov9597@gmail.com>
2025-10-10 22:43:27 +0300

    sorted_array_dumbest.py done

--- sorted_array_dumbest.py
+++ sorted_array_dumbest.py
@@ -1,14 +1,21 @@
 class Solution:
     def searchMin(self, nums: list[int]):
         min = None
+        min_i = None
         for i in range(len(nums)):
             if min is None or nums[i] < min:
                 min = nums[i]
+                min_i = i
+        return min_i
     def sortedArrayDumbest(self, nums: list[int]) -> list[int]:
-        SortArr = ()
-        min = None
+        sorted_arr = []
         while nums:
-            Solution().searchMin(nums)
+            min_i = self.searchMin(nums)
+            sorted_arr.append(nums.pop(min_i))
+        return sorted_arr
+
+result = Solution().sortedArrayDumbest([1, 20, 3, 3, 21])
+print(result)
 
 
         #Поиск индекса минимума можно выделить в отдельную функцию, по нему можно будет попать элемент, потом добавить его в новый список, повторять пока изначальный список не опустеет
