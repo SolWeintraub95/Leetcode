2f600fcdb4e50d96a9c2e6bf884f16e87e2119df
SolWeintraub95 <shutov9597@gmail.com>
2025-10-12 18:38:56 +0300

    sorted_array_bubble.py started

---
+++ sorted_array_bubble.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortedArrayBubble(self, nums: list[int]) -> list[int]:
+        #Нужно два цикла, первый будет переводить индексы ближе к 0 и сравнивать по новой, а второй будет проходиться по "парам" и сравнивать уже их
+
+
+
+
+
+
+
+
+
+result = Solution().sortedArrayBubble([1, 201, 3, 3, 21])
+print(result)
