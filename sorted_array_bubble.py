dbd83c6d32cf7bd015b1d7ce8f9307bc559d8d7c
SolWeintraub95 <shutov9597@gmail.com>
2025-10-16 21:10:52 +0300

    sorted_array_bubble.py is done with Pete

--- sorted_array_bubble.py
+++ sorted_array_bubble.py
@@ -1,14 +1,17 @@
 class Solution:
-	def sortedArrayBubble(self, nums: list[int]) -> list[int]:
-		for i in range(len(nums)-1):
-			isSorted = True
-			for k in range(len(nums)-i-1):
-				if nums[k] > nums[k+1]:
-					nums[k], nums[k+1] = nums[k+1], nums[k]
-					isSorted = False
-			if isSorted:
-				break
-		return nums
+    def sortedArrayBubble(self, nums: list[int]) -> list[int]:
+        for i in range(len(nums) - 1):
+            isSorted = True
+            for k in range(len(nums) - i - 1):
+                if nums[k] > nums[k + 1]:
+                    nums[k], nums[k + 1] = nums[k + 1], nums[k]
+                    isSorted = False
+        # print(nums)
+            if isSorted:
+                break
+
+        return nums
+
 
 result = Solution().sortedArrayBubble([3, 2, 1, 200, 4, -1, 0])
 print(result)
